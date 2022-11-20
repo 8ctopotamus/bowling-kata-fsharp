093d9bcfe94411fadd01af7d1532bc6572cc6a63
8ctopotamus <joshnaylor88@gmail.com>
2022-11-20 15:56:46 -0600

    Roll type and generate frames

--- Program.fs
+++ Program.fs
@@ -1,2 +1,23 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿type Roll =
+  | Empty of int
+  | PINS of int
+  | SPARE of string
+  | STRIKE of string
+
+  static member ToString () =
+    "test"
+
+let setUpFrames () = 
+  (Empty 0, Empty 0)
+
+[<EntryPoint>]
+let main argv =
+  
+  let frames = 
+    (Array.create 10 (PINS 0, PINS 0))
+    |> Array.map (fun f -> Roll.ToString())
+
+
+  printfn "%A" frames
+
+  0
