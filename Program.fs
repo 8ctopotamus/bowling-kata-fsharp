19936e7ab2fd58daf2d8ccb2f064cdf89aa2dad5
Josh <zylo.codes@gmail.com>
2022-11-20 16:55:48 -0600

    Use reflection to get Roll union cases

--- Program.fs
+++ Program.fs
@@ -1,23 +1,35 @@
-﻿type Roll =
+﻿open System
+open FSharp.Reflection
+
+let rand = Random()
+
+type Roll =
   | Empty of int
   | PINS of int
-  | SPARE of string
-  | STRIKE of string
+  | SPARE of int
+  | STRIKE of int
+  
+  // member x.get() =
+    // x.GetUnionCases(typeof<'T>)
 
-  static member ToString () =
-    "test"
+type Frame = Tuple<Roll, Roll>
 
-let setUpFrames () = 
-  (Empty 0, Empty 0)
+// let throwBall (frame: Frame) =
+//   PINS 1, PINS 3
 
 [<EntryPoint>]
-let main argv =
+let main argv: int =
   
-  let frames = 
-    (Array.create 10 (PINS 0, PINS 0))
-    |> Array.map (fun f -> Roll.ToString())
+  let frames: Frame[] = 
+    (Array.create 10 (Empty 0, Empty 0))
+    // |> Array.map throwBall
 
 
-  printfn "%A" frames
+  let recordType = typedefof<Roll>
+  let cases = FSharpType.GetUnionCases(recordType)
+  printfn "%A" cases 
+  // let c = FSharpType.GetUnionCases <Roll>
 
+  // printfn "%A" frames
+  
   0
